12c6cd2d5f66d07322a57f4aae509c7025b4e347
Harshit Joshi <harshitjoshi250@gmail.com>
2020-07-11 13:12:27 +0530

    Udacity Quiz App

--- app/src/main/java/com/example/myquiz/MainActivity.java
+++ app/src/main/java/com/example/myquiz/MainActivity.java
@@ -14,120 +14,96 @@ import android.widget.Toast;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    int score=0;
-    int countRight=0;
-
+    private int score = 0;
+    private int countRight = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void magic(View view){
-        if(getAnswerOne()){
-            countRight+=1;
+
+    public void submit(View view) {
+        if (getAnswerOne()) {
+            countRight += 1;
         }
-
-
-        if(getAnswerTwo()){
-            countRight+=1;
+        if (getAnswerTwo()) {
+            countRight += 1;
         }
-
-
-        if(getAnswerThree()){
-            countRight+=1;
-
+        if (getAnswerThree()) {
+            countRight += 1;
         }
-        if(getAnswerFour()){
-            countRight+=1;
+        if (getAnswerFour()) {
+            countRight += 1;
         }
-        if(getAnswerFive()){
-            countRight+=1;
+        if (getAnswerFive()) {
+            countRight += 1;
         }
-
-
-
-        score=countRight;
-
-
-
-
-
-        Toast.makeText(this, " Your score is " +score+ " Right Answers = " +countRight+ " Wrong Answers = " + (5-score)+ "" ,Toast.LENGTH_LONG).show();
-
-
-
+        score = countRight;
+        Toast.makeText(this, " Your score is " + score + " Right Answers = " + countRight + " Wrong Answers = " + (5 - score) + "", Toast.LENGTH_LONG).show();
     }
 
-    public boolean getAnswerOne(){
-        String rightAnswerOne ="Albert Einstein";
-        EditText answerOne=(EditText)findViewById(R.id.edit);
-        String myAnswerOne=answerOne.getText().toString();
-        if(myAnswerOne.equals(rightAnswerOne)){
-            return  true;
-        }
-        return false;
-    }
-    public boolean getAnswerTwo(){
-        String rightAnswerTwo="Stefan Hawking";
-        EditText answerTwo=(EditText)findViewById(R.id.editTwo);
-        String myAnswertwo=answerTwo.getText().toString();
-        if(myAnswertwo.equals(rightAnswerTwo)){
+    public boolean getAnswerOne() {
+        String rightAnswerOne = "Albert Einstein";
+        EditText answerOne = (EditText) findViewById(R.id.edit);
+        String myAnswerOne = answerOne.getText().toString();
+        if (myAnswerOne.equalsIgnoreCase(rightAnswerOne)) {
             return true;
         }
         return false;
     }
 
-    public boolean getAnswerThree(){
-        CheckBox checkOne=(CheckBox)findViewById(R.id.firstCheckBox);
-        boolean hasCheckOne=checkOne.isChecked();
-
-
-
-        CheckBox checkTwo=(CheckBox)findViewById(R.id.secondCheckBox);
-        boolean hasCheckSecond=checkTwo.isChecked();
-
-
-
-        CheckBox checkThree=(CheckBox)findViewById(R.id.thirdCheckBox);
-        boolean hasCheckThird=checkThree.isChecked();
-
-        if((hasCheckOne) && (hasCheckSecond)&&(!hasCheckThird)){
-            return  true;
+    public boolean getAnswerTwo() {
+        String rightAnswerTwo = "Stefan Hawking";
+        EditText answerTwo = (EditText) findViewById(R.id.editTwo);
+        String myAnswertwo = answerTwo.getText().toString();
+        if (myAnswertwo.equalsIgnoreCase(rightAnswerTwo)) {
+            return true;
         }
-            return false;
-
-
+        return false;
     }
-    public boolean getAnswerFour(){
+
+    public boolean getAnswerThree() {
+        CheckBox checkOne = (CheckBox) findViewById(R.id.firstCheckBox);
+        boolean hasCheckOne = checkOne.isChecked();
+
+        CheckBox checkTwo = (CheckBox) findViewById(R.id.secondCheckBox);
+        boolean hasCheckSecond = checkTwo.isChecked();
+
+        CheckBox checkThree = (CheckBox) findViewById(R.id.thirdCheckBox);
+        boolean hasCheckThird = checkThree.isChecked();
+
+        if ((hasCheckOne) && (hasCheckSecond) && (!hasCheckThird)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean getAnswerFour() {
 
 
-        RadioButton heckYes=(RadioButton)findViewById(R.id.yes_button);
-        boolean myHeckYes=heckYes.isChecked();
-
-
-
+        RadioButton heckYes = (RadioButton) findViewById(R.id.yes_button);
+        boolean myHeckYes = heckYes.isChecked();
 
         RadioButton heckNo = (RadioButton) findViewById(R.id.no_button);
         boolean myHeckNo = heckNo.isChecked();
-        if(myHeckYes){
+        if (myHeckYes) {
             return true;
         }
         return false;
 
     }
+
     public boolean getAnswerFive() {
-        RadioButton heckYesTwo= (RadioButton) findViewById(R.id.yesTwo_button);
+        RadioButton heckYesTwo = (RadioButton) findViewById(R.id.yesTwo_button);
         boolean myHeckYesTwo = heckYesTwo.isChecked();
 
-
-
-        RadioButton heckNoTwo= (RadioButton) findViewById(R.id.noTwo_button);
-        boolean myHeckNoTwo= heckNoTwo.isChecked();
-      if(myHeckYesTwo){
-          return true;
-      }
-      return false;
+        RadioButton heckNoTwo = (RadioButton) findViewById(R.id.noTwo_button);
+        boolean myHeckNoTwo = heckNoTwo.isChecked();
+        if (myHeckYesTwo) {
+            return true;
+        }
+        return false;
     }
 
 }
